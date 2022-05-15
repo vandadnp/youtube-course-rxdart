0743259f5bba00861899ed675649c9470136d297
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-05-15 05:16:27 +0200

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:testingrxdart_course/views/home_page.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
@@ -20,6 +25,39 @@ class App extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
+    );
+  }
+}
+
+void testIt() async {
+  final stream1 = Stream.periodic(
+    const Duration(seconds: 1),
+    (count) => 'Stream 1, count = $count',
+  );
+  final stream2 = Stream.periodic(
+    const Duration(seconds: 3),
+    (count) => 'Stream 2, count = $count',
+  );
+  final combined = Rx.combineLatest2(
+    stream1,
+    stream2,
+    (one, two) => 'One = ($one), two = ($two)',
+  );
+  await for (final value in combined) {
+    value.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
     );
   }
 }
--- lib/models/thing.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart' show immutable;
-
-@immutable
-class Thing {
-  final String name;
-  const Thing({
-    required this.name,
-  });
-}
