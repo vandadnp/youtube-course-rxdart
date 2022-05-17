fb82c8d781285787fd841605151ff4c24656f1c1
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-05-17 05:13:07 +0200

    step-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
@@ -24,157 +27,51 @@ class App extends StatelessWidget {
   }
 }
 
-@immutable
-class Bloc {
-  final Sink<String?> setFirstName; // write-only
-  final Sink<String?> setLastName; // write-only
-  final Stream<String> fullName;
-
-  const Bloc._({
-    required this.setFirstName,
-    required this.setLastName,
-    required this.fullName,
-  });
-
-  void dispose() {
-    setFirstName.close();
-    setLastName.close();
-  }
-
-  factory Bloc() {
-    final firstNameSubject = BehaviorSubject<String?>();
-    final lastNameSubject = BehaviorSubject<String?>();
-
-    final Stream<String> fullName = Rx.combineLatest2(
-        firstNameSubject.startWith(null), lastNameSubject.startWith(null),
-        (String? firstName, String? lastName) {
-      if (firstName != null &&
-          firstName.isNotEmpty &&
-          lastName != null &&
-          lastName.isNotEmpty) {
-        return '$firstName $lastName';
-      } else {
-        return 'Both first and last name must be provided';
-      }
-    });
-
-    return Bloc._(
-      setFirstName: firstNameSubject.sink,
-      setLastName: lastNameSubject.sink,
-      fullName: fullName,
-    );
-  }
+Stream<String> getNames({
+  required String filePath,
+}) {
+  final names = rootBundle.loadString(filePath);
+  return Stream.fromFuture(names).transform(const LineSplitter());
 }
 
-typedef AsyncSnapshotBuilderCallback<T> = Widget Function(
-  BuildContext context,
-  T? value,
-);
+Stream<String> getAllNames() => getNames(filePath: 'assets/texts/cats.txt')
+    .concatWith([getNames(filePath: 'assets/texts/dogs.txt')]).delay(
+        const Duration(seconds: 3));
 
-class AsyncSnapshotBuilder<T> extends StatelessWidget {
-  final Stream<T> stream;
-  final AsyncSnapshotBuilderCallback<T>? onNone;
-  final AsyncSnapshotBuilderCallback<T>? onWaiting;
-  final AsyncSnapshotBuilderCallback<T>? onActive;
-  final AsyncSnapshotBuilderCallback<T>? onDone;
-
-  const AsyncSnapshotBuilder({
-    Key? key,
-    required this.stream,
-    this.onNone,
-    this.onWaiting,
-    this.onActive,
-    this.onDone,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<T>(
-      stream: stream,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            final callback = onNone ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-          case ConnectionState.waiting:
-            final callback =
-                onWaiting ?? (_, __) => const CircularProgressIndicator();
-            return callback(
-              context,
-              snapshot.data,
-            );
-          case ConnectionState.active:
-            final callback = onActive ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-          case ConnectionState.done:
-            final callback = onDone ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-        }
-      },
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final Bloc bloc;
-  @override
-  void initState() {
-    super.initState();
-    bloc = Bloc();
-  }
-
-  @override
-  void dispose() {
-    bloc.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CombineLatest with RxDart'),
+        title: const Text('Home Page'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Enter first name here...',
-              ),
-              onChanged: bloc.setFirstName.add,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Enter last name here...',
-              ),
-              onChanged: bloc.setLastName.add,
-            ),
-            AsyncSnapshotBuilder<String>(
-              stream: bloc.fullName,
-              onActive: (context, String? value) {
-                return Text(value ?? '');
-              },
-            )
-          ],
-        ),
+      body: FutureBuilder<List<String>>(
+        future: getAllNames().toList(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.done:
+              final names = snapshot.requireData;
+              return ListView.separated(
+                separatorBuilder: (_, __) => const Divider(
+                  color: Colors.black,
+                ),
+                itemCount: names.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(names[index]),
+                  );
+                },
+              );
+          }
+        },
       ),
     );
   }
